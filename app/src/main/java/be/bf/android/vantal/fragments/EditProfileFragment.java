7c977dce733e7e94af97455b60eb35c578740347
Pierre Breny <pierrebreny@Tardis.local>
2022-09-21 17:14:01 +0200

    Profile & Edit Profile frags DONE

---
+++ app/src/main/java/be/bf/android/vantal/fragments/EditProfileFragment.java
@@ -0,0 +1,35 @@
+package be.bf.android.vantal.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import be.bf.android.vantal.R;
+
+public class EditProfileFragment extends Fragment {
+
+    public EditProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static EditProfileFragment newInstance(String param1, String param2) {
+        EditProfileFragment fragment = new EditProfileFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+    }
+}
